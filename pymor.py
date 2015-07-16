30d8f69e4524ed19e272b5fd9079ca43eb4a1a9f
tim <tim@MacBook-Pro-tim.local>
2015-07-16 17:14:46 +0300

    encode result to json

--- pymor.py
+++ pymor.py
@@ -1,7 +1,10 @@
 import sys
+import json
 import pymorphy2
 from pipe import * 
 @Pipe
 def to_set(x): return set(x)
+@Pipe
+def encode(x): return json.dumps(x, ensure_ascii=False)
 morph = pymorphy2.MorphAnalyzer()
-map(lambda x: map(lambda p: map(lambda w: w.word, p.lexeme), morph.parse(x)), (sys.argv | skip(1))) | traverse | as_list | to_set | as_list | stdout
+map(lambda x: map(lambda p: map(lambda w: w.word, p.lexeme), morph.parse(x)), (sys.argv | skip(1))) | traverse | as_list | to_set | as_list | encode | stdout
